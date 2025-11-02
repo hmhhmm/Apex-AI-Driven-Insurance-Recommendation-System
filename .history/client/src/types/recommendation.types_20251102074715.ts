2ec5ad35bc30671029fcad85b1f72cbbc4da6c44
fciacia <flc.066734@gmail.com>
2025-11-02 09:19:39 +0800

    Real live data

---
+++ .history/client/src/types/recommendation.types_20251102074715.ts
@@ -0,0 +1,31 @@
+import { DNAInterpretation } from './dna.types';
+import { PlanRecommendation } from './insurance.types';
+import { RiskProfile } from './user.types';
+
+export interface RecommendationRequest {
+  userId: string;
+  userProfile: any;
+}
+
+export interface RecommendationResponse {
+  success: boolean;
+  data: {
+    userRiskProfile: RiskProfile & {
+      dnaInterpretation: DNAInterpretation;
+    };
+    recommendations: PlanRecommendation[];
+    alternativePlans: PlanRecommendation[];
+    totalPotentialSavings: number;
+    confidence: number;
+    aiInsights?: {
+      overallAnalysis: string;
+      riskFactors: string[];
+      savingsTips: string[];
+    };
+  };
+}
+
+export interface SavedPlan {
+  planId: string;
+  savedAt: string;
+}
